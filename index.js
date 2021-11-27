843b3cfda152dfda43b8534e314da0264efa41cf
Victor Turitzin <turitzinv@gmail.com>
2021-11-27 12:27:53 -0500

    commit tree cb9c4ffe9767a9d3b415e9ddcdac4fad0cb2b851

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array){
+    //const array = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array){
+    //const array = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    return array.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ function createFareMultiplier(number){
+     return function(fare){
+        return fare * number
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+let drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    if (selectingDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers) 
+        } else {
+            return returnLastTwoDrivers(drivers);
+        }
+}
